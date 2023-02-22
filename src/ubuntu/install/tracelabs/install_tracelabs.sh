25f6760eeb4d4e9e31e88afeace6d84cea15cdf6
Tom Hocker <tom@humandecoded.io>
2023-02-22 16:50:19 -0500

    modified for new build process

--- src/ubuntu/install/tracelabs/install_tracelabs.sh
+++ src/ubuntu/install/tracelabs/install_tracelabs.sh
@@ -3,42 +3,28 @@ set -e
 set -x
 
 cd /tmp/
-git clone https://github.com/tracelabs/tlosint-live.git
-cd /tmp/tlosint-live/
+git clone https://github.com/tracelabs/tlosint-vm.git
+cd /tmp/tlosint-vm/
+git checkout tom-no-tools
 
 #### Setup Desktop Icons, backgrounds, etc ####
-rsync -aviu kali-config/common/includes.chroot/etc/ /etc/
-rsync -aviu kali-config/common/includes.chroot/usr/ /usr/
+rsync -aviu overlays/tl-overlays/etc/ /etc/
+rsync -aviu overlays/tl-overlays/usr/ /usr/
 
-mv /etc/skel/Desktop/*.pdf $HOME/Desktop/
+mv /etc/skel/Desktop/* $HOME/Desktop/
 
-#### Install all tracelabs image packages ####
-apt-get update
-#                                                              rm lines with # | Delete Empty lines | 
-cat kali-config/variant-tracelabs/package-lists/kali.list.chroot | sed '/^#/d' | sed '/^$/d' | xargs --no-run-if-empty apt-get install -y
+# get rid of kali default
+apt update
+apt remove kali-linux-default -y
+apt autoremove -y
 
-sh kali-config/common/hooks/normal/osint-packages.chroot
-
-chown -R 1000:1000 \
-    /usr/share/phoneinfoga \
-    /usr/share/Spiderpig \
-    /usr/share/DumpsterDiver \
-    /usr/share/Infoga \
-    /usr/share/LittleBrother \
-    /usr/share/sn0int \
-    /usr/share/buster \
-    /usr/share/sherlock \
-    /usr/share/reconspider \
-    /usr/share/WhatsMyName \
-    /usr/share/WikiLeaker \
-    /usr/share/OnionSearch \
-    /usr/share/toutatis 
+#install tl tools
+sh $HOME/Desktop/install-tools.sh
 
 apt-get install -y python3-pip
 
 pip3 install --force-reinstall zope.event
 
-sed -i 's/sudo //g' /usr/share/applications/tl*.desktop
 
 rm -rf /var/lib/apt/lists/*
 
